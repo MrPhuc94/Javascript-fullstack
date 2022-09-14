8f7ecfa26b3cebd60eebcb8cd0227816549e8330
MrPhuc94 <phuc@republicpower.net>
2022-09-14 11:55:11 +0700

    FIRST COMMIT

---
+++ react-webpack/webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js", // Dẫn tới file index.js ta đã tạo
+  output: {
+    path: path.join(__dirname, "/build"), // Thư mục chứa file được build ra
+    filename: "bundle.js", // Tên file được build ra
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+      {
+        test: /\.css$/, // Sử dụng style-loader, css-loader cho file .css
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  // Chứa các plugins sẽ cài đặt trong tương lai
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};
